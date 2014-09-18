37d6fdae93608a94f27b5a416adda7157e55c8b8
slavatour <dzvinok.cv@gmail.com>
2014-09-18 22:53:01 +0300

    add event to product model view

--- public/javascripts/views/productModelView.js
+++ public/javascripts/views/productModelView.js
@@ -3,7 +3,15 @@ define(["Store", "marionette", "views/spinnerView"], function (Store, Marionette
     Store.module("Products.Views", function (Views, Store, Backbone, Marionette, $, _) {
 		Views.ProductModelView = Backbone.Marionette.ItemView.extend({
 			template: "#productModelTemplate",
-            tagName: "tr"
+            tagName: "tr",
+            events: {
+                "click .toggleTable": "toggleTable"
+            },
+            toggleTable: function(event) {
+                $target = $(event.target);
+                console.log($target.find("table"));
+                $target.parent().next("table").hasClass("hide") ? $target.parent().next("table").removeClass("hide") : $target.parent().next("table").addClass("hide");
+            }
 		});
 	});
     return Store.Products.Views.ProductModelView;
