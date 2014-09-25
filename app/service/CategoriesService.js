2bf6db12089ef72c56a0a0f23759984a98874d59
slavatour <dzvinok.cv@gmail.com>
2014-09-25 23:57:07 +0300

    add checkbox widget button

--- app/service/CategoriesService.js
+++ app/service/CategoriesService.js
@@ -7,17 +7,25 @@ exports.CategoriesService = function (conString) {
         var categoriesRepository = new CategoriesRepository.CategoriesRepository(conString);
         categoriesRepository.fetchCategories(function(options){
             var categories = options.result;
+            console.log(categories);
             for(var i=0, length = categories.length; i<length; i++) {
                 categories[i].subcategories = [];
                 for(var j=0, lengthSub = categories.length; j<lengthSub; j++) {
                     if(categories[j].category_parent_id == categories[i].category_id) {
-                        //MYDOTO !!!!! finish function
                         categories[i].subcategories.push(categories[j]);
                     }
                 }
             }
             callbackFunction(categories);
         });
+    };
+    function _recursive (collection) {
+        var categories = [],
+            subcategories = [];
+        for (var i= 0, length = collection.length; i < length; i++) {
+            collection[i]["category_parent_id"] == null ? categories.push(collection[i]) : subcategories.push(collection[i]);
+        }
+
     }
 
     return self;
